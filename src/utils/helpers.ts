f31780edba27193086268515dd79aa82b02e3a54
Ebenezer Nikabou <nikaboue10@gmail.com>
2023-10-28 21:53:37 +0300

    fixed path resolve bug

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,12 +1,9 @@
 import { PathType } from './parser.js'
 import path from 'path'
-import { fileURLToPath } from 'url'
 import fs from 'fs'
 
-const _filename = fileURLToPath(import.meta.url)
-const _dirname = path.dirname(_filename)
 export const isFolderOrFile = (filePath: string): PathType => {
-  filePath = path.join(_dirname, filePath)
+  filePath = path.resolve(filePath)
 
   if (!fs.lstatSync(filePath).isDirectory() && !fs.lstatSync(filePath).isFile()) {
     throw new Error('Path is neither a folder nor a file')
